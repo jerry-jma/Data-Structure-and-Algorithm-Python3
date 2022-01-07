fff19cf660d188a122fb23fed54fe9345b969e29
junma <junma0704@gmail.com>
2022-01-06 18:51:54 -0800

    Updated Same Tree

--- Trees/Same Tree.py
+++ Trees/Same Tree.py
@@ -49,4 +49,4 @@ class Solution:
           return False
       if a.val != b.val:
           return False
-      return self.isIdentical(a.left, b.left) + self.isIdentical(a.right, b.right)
+      return self.isIdentical(a.left, b.left) and self.isIdentical(a.right, b.right)
