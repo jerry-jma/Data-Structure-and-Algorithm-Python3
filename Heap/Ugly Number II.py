f12c016ecdd929ebe50dc40650288b8a49057fe7
junma <junma0704@gmail.com>
2022-02-15 23:34:40 -0800

    Ugly Number II Using DP

--- Heap/Ugly Number II.py
+++ Heap/Ugly Number II.py
@@ -41,17 +41,34 @@ class Solution:
     @param n: An integer
     @return: return a  integer as description.
     """
+    # def nthUglyNumber(self, n):
+    #     heap = [1]
+    #     seen = set([1])
+    #     curr_ugly = 1
+
+    #     for _ in range(n):
+    #         curr_ugly = heapq.heappop(heap)
+    #         for factor in [2,3,5]:
+    #             new_ugly = curr_ugly * factor
+    #             if new_ugly not in seen:
+    #                 seen.add(new_ugly)
+    #                 heapq.heappush(heap, new_ugly)
+
+    #     return curr_ugly
+
+    # solution 2: Using DP
     def nthUglyNumber(self, n):
-        heap = [1]
-        seen = set([1])
-        curr_ugly = 1
+        dp = [0] * n
+        dp[0] = 1
+        l2, l3, l5 = 0, 0, 0
 
-        for _ in range(n):
-            curr_ugly = heapq.heappop(heap)
-            for factor in [2,3,5]:
-                new_ugly = curr_ugly * factor
-                if new_ugly not in seen:
-                    seen.add(new_ugly)
-                    heapq.heappush(heap, new_ugly)
+        for i in range(1, n):
+            dp[i] = min(2*dp[l2], 3*dp[l3], 5*dp[l5])
+            if dp[i] == 2 * dp[l2]:
+                l2 += 1
+            if dp[i] == 3 * dp[l3]:
+                l3 += 1
+            if dp[i] == 5 * dp[l5]:
+                l5 += 1
 
-        return curr_ugly
+        return dp[-1]
