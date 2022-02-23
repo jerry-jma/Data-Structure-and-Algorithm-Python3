e49f8075d86b24901ae547860db157c418e6b136
junma <junma0704@gmail.com>
2022-02-22 19:57:23 -0800

    Triangle using dp

--- Dynamic Programing/Triangle.py
+++ Dynamic Programing/Triangle.py
@@ -71,6 +71,23 @@ class Solution:
     #     return memo[(x,y)]
 
 
+        # solution 2: using DP
     def minimum_total(self, triangle: List[List[int]]) -> int:
+        if not triangle or not triangle[0]:
+            return -1
+
+        n = len(triangle)
+        dp = [[0]*n for _ in range(n)]
+
+        dp[0][0] = triangle[0][0]
+
+        for i in range(1, n):
+            dp[i][0] = dp[i-1][0] + triangle[i][0]
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i]
+            print(i, i-1)
+            for j in range(1, i):
+                dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
+
+        return min(dp[n-1])
 
 
