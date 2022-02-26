3e2c8fb2f35a291a44e5102408695bc2623d9340
junma <junma0704@gmail.com>
2022-02-25 16:19:01 -0800

    Subsets Using DFS and Backtracking

---
+++ DFS/Subsets.py
@@ -0,0 +1,67 @@
+# Description
+# Given a set with distinct integers, return all possible subsets.
+
+# Wechat reply the【BAT】get the latest frequent Interview questions of ByteDance, Alibaba, etc. (wechat id :jiuzhang15)
+
+# Elements in a subset must be in non-descending order.
+# The solution set must not contain duplicate subsets.
+# Example
+# Example 1:
+
+# Input:
+
+# nums = [0]
+# Output:
+
+# [
+#   [],
+#   [0]
+# ]
+# Explanation:
+
+# The subsets of [0] are only [] and [0].
+
+# Example 2:
+
+# Input:
+
+# nums = [1,2,3]
+# Output:
+
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+# Explanation:
+
+# The subsets of [1,2,3] are [], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3].
+
+# Challenge
+# Can you do it in both recursively and non-recursively?
+
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+    """
+    def subsets(self, nums):
+       nums = sorted(nums)
+       combinations = []
+       self.dfs(nums, 0, [], combinations)
+       return combinations
+
+    def dfs(self, nums, index, combination, combinations):
+        # or use list(combination) to create a new copy
+        combinations.append(combination[:])
+
+        for i in range(index, len(nums)):
+            combination.append(nums[i])
+            self.dfs(nums, i+1, combination, combinations)
+            combination.pop()
+
