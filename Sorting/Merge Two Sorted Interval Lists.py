18ce1190ab43a2b6f530c6c3c5778bfb89a088fd
junma <junma0704@gmail.com>
2022-03-02 23:23:05 -0800

    Merge two sorted interval lists using brute force

--- Sorting/Merge Two Sorted Interval Lists.py
+++ Sorting/Merge Two Sorted Interval Lists.py
@@ -21,3 +21,46 @@
 # (1,2) --> (1,2)
 # (3,4),(4,5) --> (3,5)
 # (6,7) --> (6,7)
+
+from typing import (
+    List,
+)
+from lintcode import (
+    Interval,
+)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param list1: one of the given list
+    @param list2: another list
+    @return: the new sorted list of interval
+    """
+    def merge_two_interval(self, list1: List[Interval], list2: List[Interval]) -> List[Interval]:
+        intervals = list1 + list2
+
+        if not intervals:
+            return []
+
+        result = []
+        prev = None
+
+        intervals = sorted(intervals, key=lambda interval:interval.start)
+
+        for interval in intervals:
+            if not prev or interval.start > prev.end:
+                result.append(interval)
+                prev = result[-1]
+            else:
+                prev.end = max(prev.end, interval.end)
+
+        return result
+
+
